8eae45db5d9213bcaa21c2100f8c5246ba65840e
Milan Patel <milanrpatel30@gmail.com>
2023-10-31 11:04:50 +0530

    added home route

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+const Listing = require("./models/listing.js");
 
 const dbUrl = process.env.ATLASDB_URL;
 
@@ -76,10 +77,15 @@ app.use ((req, res, next)=>{
   next();
 });
 
+app.get('/',  async(req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", {allListings});
+});
 
 app.use("/listings", listingRoute);
 app.use("/listings/:id/reviews", reviewRoute);
 app.use("/", userRoute);
+
 
 
 app.all("*", (req, res, next)=>{
