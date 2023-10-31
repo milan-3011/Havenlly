e6d35996c47d8e64a70b33d1d5e11d55acae1f67
Milan Patel <milanrpatel30@gmail.com>
2023-10-31 19:19:24 +0530

    maptoken upadted

--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = process.env.MAP_TOKEN;
+mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
